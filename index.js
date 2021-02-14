fd73fc7399795dc789a5d393a1c399b1faa7d9f6
sweelt0ve98 <loveparadise98@gmail.com>
2021-02-15 05:24:44 +0700

    add chat with all user feature

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const app = require('express')()
 const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '\\index.html')
+})
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit("new user connected")
+    socket.on('disconnect', () => {
+        socket.broadcast.emit("user disconnected")
+    })
+    socket.on('chat message', (message) => {
+        io.emit('chat message', message)
+    })
 })
 
 http.listen(3000, () => {
